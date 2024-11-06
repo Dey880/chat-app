17ffb0a7875d463bf571c80d517c8b2b068d010f
Dey880 <day4y3@proton.me>
2024-11-06 15:25:10 +0100

    styled login, createuser and about

--- chat-app/src/pages/CreateUser.js
+++ chat-app/src/pages/CreateUser.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import styles from "../css/CreateUser.module.css";
 
 export default function CreateUser() {
     const [email, setEmail] = useState("");
@@ -35,18 +36,52 @@ export default function CreateUser() {
 
     return (
         <>
-            <h1>Create User</h1>
-            <form onSubmit={handleSubmit}> 
-                <input type='email' placeholder='email' onChange={(e) => setEmail(e.target.value)} />
-                <input type='password' placeholder='password' onChange={(e) => setPassword(e.target.value)} />
-                <input type='password' placeholder='repeat password' onChange={(e) => setRepeatPassword(e.target.value)} />
-                <select onChange={(e) => setRole(e.target.value)} value={role}>
-                    <option value="user">User</option>
-                    <option value="admin">Admin</option>
-                    <option value="moderator">Moderator</option>
-                </select>
-                <button type="submit">Register User</button>
+        <div className={styles.createUserAboutButtonContainer}>
+            <a href='/about' className={styles.createUserAboutA}>
+                <button className={styles.createUserAboutButton}> About us </button>
+            </a>
+        </div>
+
+        <ul className={styles.createUserLoginUl}>
+            <li className={styles.createUserLoginLi}>Welcome</li>
+            <li className={styles.createUserLoginLi}>To</li>
+            <li className={styles.createUserLoginLi}><strong>CHATTER’N</strong></li>
+        </ul>
+        <hr className={styles.createUserLoginHr}/>
+        <h1 className={styles.createUserLoginText}>Sign up</h1>
+        <div className={styles.createUserLoginFormContainer}>
+            <form onSubmit={handleSubmit} className={styles.createUserLoginForm}> 
+                <input className={`${styles.createUserLoginEmail} ${styles.createUserLoginInput}`} type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+                <input className={`${styles.createUserLoginPassword} ${styles.createUserLoginInput}`} type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+                <input className={`${styles.createUserLoginRepeatPassword} ${styles.createUserLoginInput}`} type='password' placeholder='Repeat password' onChange={(e) => setRepeatPassword(e.target.value)} />
+                
+                <div className={styles.createUserRadioInput}>
+                    <label>
+                        <input type="radio" id="user" name="role-radio" value="user" checked={role === "user"} onChange={() => setRole("user")} />
+                        <span>User</span>
+                    </label>
+                    <label>
+                        <input type="radio" id="admin" name="role-radio" value="admin" checked={role === "admin"} onChange={() => setRole("admin")} />
+                        <span>Admin</span>
+                    </label>
+                    <label>
+                        <input type="radio" id="moderator" name="role-radio" value="moderator" checked={role === "moderator"} onChange={() => setRole("moderator")} />
+                        <span>Moderator</span>
+                    </label>
+                    <span className={styles.createUserSelection}></span>
+                </div>
+
+                <button className={`${styles.createUserLoginSubmit} ${styles.createUserLoginInput}`} type="submit">Create Account</button>
             </form>
+        </div>
+        <h1 className={styles.createUserLoginH1}>Already have an account? <a className={styles.createUserLoginA} href='/login'>Login</a></h1>
+        <div className={styles.createUserInfoContainer}>
+            <h2 className={styles.createUserInfoH2}>Bla info bla bla</h2>
+            <div className={styles.createUserInfoDivContainer}>
+                <div className={`${styles.createUserInfoDiv1} ${styles.createUserInfoDiv}`}></div>
+                <div className={`${styles.createUserInfoDiv2} ${styles.createUserInfoDiv}`}></div>
+            </div>
+        </div>
         </>
     );
 };
--- chat-app/src/pages/Login.js
+++ chat-app/src/pages/Login.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { jwtDecode } from "jwt-decode";
+import styles from "../css/Login.module.css"
 
 export default function Login() {
     const [email, setEmail] = useState("");
@@ -39,11 +40,35 @@ export default function Login() {
 
     return (
         <>
-            <form onSubmit={handleSubmit}> 
-                <input type="email" placeholder="email" onChange={(e) => setEmail(e.target.value)} />
-                <input type="password" placeholder="password" onChange={(e) => setPassword(e.target.value)} />
-                <button type="submit">Login</button>
+
+        <div className={styles.aboutButtonContainer}>
+            <a href='/about' className={styles.aboutA}>
+                <button className={styles.aboutButton}> About us </button>
+            </a>
+        </div>
+
+        <ul className={styles.loginUl}>
+            <li className={styles.loginLi}>Welcome</li>
+            <li className={styles.loginLi}>To</li>
+            <li className={styles.loginLi}><strong>CHATTER’N</strong></li>
+        </ul>
+        <hr className={styles.loginHr}/>
+        <h1 className={styles.loginText}>Login</h1>
+        <div className={styles.loginFormContainer}>
+            <form onSubmit={handleSubmit} className={styles.loginForm}> 
+                <input className={`${styles.loginEmail} ${styles.loginInput}`} type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+                <input className={`${styles.loginPassword} ${styles.loginInput}`} type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+                <button className={`${styles.loginSubmit} ${styles.loginInput}`} type="submit">Login</button>
             </form>
+        </div>
+        <h1 className={styles.loginH1}>Don't have an account? <a className={styles.loginA} href='/createUser'>Sign up</a></h1>
+        <div className={styles.infoContainer}>
+            <h2 className={styles.infoH2}>Bla info bla bla</h2>
+            <div className={styles.infoDivContainer}>
+                <div className={`${styles.infoDiv1} ${styles.infoDiv}`}></div>
+                <div className={`${styles.infoDiv2} ${styles.infoDiv}`}></div>
+            </div>
+        </div>
         </>
     );
 };
